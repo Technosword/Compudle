25be2e04e4d9246f6fd387e82785a1dcd71334e0
Technosword <tobyrschwartz@gmail.com>
2022-03-14 10:25:07 -0400

    Fix bugs and add deletion. Also expanded word list

--- src/main/java/us/techno/listeners/KeyPressListener.java
+++ src/main/java/us/techno/listeners/KeyPressListener.java
@@ -23,6 +23,7 @@ public class KeyPressListener implements KeyListener {
                 int index = game.getSquareIndex();
                 String keyPressed = String.valueOf(e.getKeyChar()).toUpperCase(Locale.ROOT);
                 boolean isTimeToGuess = index == 4 || index == 9 || index == 14 || index == 19 || index == 24 || index == 29;
+                System.out.println(index);
                 if (e.getKeyCode() == KeyEvent.VK_ENTER) {
                         if (isTimeToGuess) {
                                 try {
@@ -34,12 +35,37 @@ public class KeyPressListener implements KeyListener {
                                 return;
                         }
                 }
+
+                if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
+                        if (index == 0 || index == 5 || index == 10 || index == 15 || index == 20 || index == 25) return;
+                        if (isTimeToGuess) {
+                                game.setCanEditGuess(true);
+                                JLabel square = Arrays.stream(game.getSquares()).toList().get(index);
+                                if (square.getText().equalsIgnoreCase("")) {
+                                        JLabel correctSquare = Arrays.stream(game.getSquares()).toList().get(index - 1);
+                                        correctSquare.setText("");
+                                        game.setSquareIndex(index - 1);
+                                } else {
+                                        square.setText("");
+                                        game.setSquareIndex(index);
+                                }
+                        } else {
+                                index = index - 1;
+                                game.setSquareIndex(index);
+                                JLabel square = Arrays.stream(game.getSquares()).toList().get(index);
+                                square.setText("");
+                        }
+                        return;
+                }
                 if (!game.isCanEditGuess()) return;
+
                 if (!Arrays.stream(game.getAlphabet()).toList().contains(keyPressed)) {
                         return;
                 }
+
                 JLabel square = Arrays.stream(game.getSquares()).toList().get(index);
                 square.setText(keyPressed);
+
                 if (isTimeToGuess) {
                         game.setCanEditGuess(false);
                         return;
